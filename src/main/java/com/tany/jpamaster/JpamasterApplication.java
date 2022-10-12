67e80116af1eeb6f916aea019303e012abac4966
ByeoungJoon Jeon <juni8453@naver.com>
2022-10-12 20:59:21 +0900

    :tada: 실전! 스프링부트와 JPA 2편 시작

---
+++ src/main/java/com/tany/jpamaster/JpamasterApplication.java
@@ -0,0 +1,13 @@
+package com.tany.jpamaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpamasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpamasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tany/jpamaster/JpamasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tany.jpamaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpamasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
