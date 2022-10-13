4a83277375d7cd754de2317898433a7027798079
ByeoungJoon Jeon <juni8453@naver.com>
2022-10-13 18:58:42 +0900

    :sparkles: Order Entity 정적 팩토리 메서드 작성 및 비즈니스 로직 추가
    #descripton
    1. 주문취소 cancel()
      - 만약 배송 상태가 COMP 라면, 취소가 불가능하도록 예외 설정
      - 그게 아니라면 주문 상태를 CANCEL 로 만들고 모든 주문 아이템 목록을 하나씩 삭제하도록 구현
    
    2. 전체 주문 가격 조회 getTotalPrice()
      - 아이템 목록을 돌며 전체 주문 가격을 더해서 반환하도록 구현

--- src/main/java/com/tany/jpamaster/domain/Order.java
+++ src/main/java/com/tany/jpamaster/domain/Order.java
@@ -1,15 +1,19 @@
 package com.tany.jpamaster.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Setter
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "orders")
 public class Order {
@@ -33,6 +37,51 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    private Order(Member member, Delivery delivery, OrderStatus status, LocalDateTime orderDate) {
+        this.member = member;
+        this.delivery = delivery;
+        this.status = status;
+        this.orderDate = orderDate;
+    }
+
+    //생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order(member, delivery, OrderStatus.ORDER, LocalDateTime.now());
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+
+        return order;
+    }
+
+
+    //==비즈니스 로직==//
+
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능 합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return orderItems.stream()
+            .mapToInt(OrderItem::getTotalPrice)
+            .reduce(0, Integer::sum);
+    }
 
     // 연관관계 메서드
     public void setMember(Member member) {
