e5a9d1c0347bf6243be5004be9b4b12a2c5de590
ByeoungJoon Jeon <juni8453@naver.com>
2022-10-13 19:01:35 +0900

    :sparkles: OrderItem Entity 정적 팩토리 메서드 작성 및 비즈니스 로직 추가
    #descripton
    1. 주문취소 cancel()
      - Order 의 cancel() 메서드가 호출될 때 부모 클래스 Item 의 addStock() 을 호출해 취소된 수량만큼 다시 수량을 더할 수 있도록 구현
    
    2. 전체 주문 가격 조회 getTotalPrice()
      - 주문 가격과 갯수를 곱한 만큼 가격을 반환하도록 구현

--- src/main/java/com/tany/jpamaster/domain/OrderItem.java
+++ src/main/java/com/tany/jpamaster/domain/OrderItem.java
@@ -1,13 +1,17 @@
 package com.tany.jpamaster.domain;
 
 import com.tany.jpamaster.domain.item.Item;
+
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Setter
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "order_item")
 @Entity
 public class OrderItem {
@@ -27,4 +31,27 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+    private OrderItem(Item item, int orderPrice, int count) {
+        this.item = item;
+        this.orderPrice = orderPrice;
+        this.count = count;
+    }
+
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem(item, orderPrice, count);
+        item.removeStock(count);
+
+        return orderItem;
+    }
+
+    //비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //조회 로직
+    public int getTotalPrice() {
+        return this.orderPrice * this.count;
+    }
 }
