5fc27f53788bd1dc13a06fc1c9ea3a2788a97144
ByeoungJoon Jeon <juni8453@naver.com>
2022-10-13 18:54:38 +0900

    :sparkles: Item Entity 비즈니스 로직 추가
    #descripton
    - 재고 수량 변경에 사용되는 비즈니스 로직 addStock(), removeStock() 메서드 작성

--- src/main/java/com/tany/jpamaster/domain/item/Item.java
+++ src/main/java/com/tany/jpamaster/domain/item/Item.java
@@ -1,6 +1,8 @@
 package com.tany.jpamaster.domain.item;
 
 import com.tany.jpamaster.domain.Category;
+import com.tany.jpamaster.exception.NotEnoughStockException;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +28,21 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * 재고가 변경되는 비즈니스 로직 추가
+     * DDD관점에서 엔티티 자체가 해결할 수 있는 로직은 엔티티 안에 넣는 것이 좋다.
+     * stockQuantity를 item엔티티가 가지고 있기 때문이기도 하다.
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
