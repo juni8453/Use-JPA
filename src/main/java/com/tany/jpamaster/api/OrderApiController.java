f3c1ee776ae1d85a8ee9a86633c727b11e05d796
ByeoungJoon Jeon <juni8453@naver.com>
2022-10-15 23:35:21 +0900

    :sparkles: 주문 조회 API V3 로직 작성완료
    #descripton
    1. orderV3()
    - findAll() 이 아닌 findAllWithItem() 메서드를 새로 만들어서 Fetch Join 옵션으로 모든 LAZY 로딩 Entity 를 한 번에 받아올 수 있도록 수정
    - 컬렉션에 Fetch Join 이 붙어져 있기 때문에 데이터 뻥튀기 현상 발생 (해당 컬렉션 수 만큼 중복된 Order Entity 반환)
    
    2. orderV4()
    - distinct 옵션을 통해 중복된 Entity 반환에서 중복을 제거할 수 있도록 로직 작성
    - 페이징을 하지 않는다면 상관없지만 페이징이 들어가야하는 API 라면 컬렉션 Fetch Join 사용 시 애플리케이션 메모리 자체에서 페이징 처리를 하기 때문에 심각한 문제가 발생할 여지가 있음.

--- src/main/java/com/tany/jpamaster/api/OrderApiController.java
+++ src/main/java/com/tany/jpamaster/api/OrderApiController.java
@@ -27,14 +27,41 @@ public class OrderApiController {
     }
 
     // select Order (1)
-        // orderItem (1) LAZY 로딩
-            // Item LAZY 로딩 + orderItem.getItem().~ LAZY 초기화(영속성 컨텍스트에 값이 없으니 쿼리발생) (2)
-        // Member (1) LAZY 로딩
-        // Delivery (1) LAZY 로딩
+    // orderItem (1) LAZY 로딩
+    // Member (1) LAZY 로딩
+    // Delivery (1) LAZY 로딩
+    // orderItem.Item LAZY 로딩 + orderItem.getItem().~ LAZY 초기화(영속성 컨텍스트에 값이 없으니 쿼리발생) (2)
     // -> 총 6번의 쿼리 발생 (N + 1) 현재 Order 가 2개니 총 12번의 쿼리 발생
     @GetMapping("/api/v2/orders")
     public GlobalResponse<List<OrderDto>> orderV2() {
         List<OrderDto> dtos = orderRepository.findAll().stream()
+            .map(order -> order.toOrderDto(order))
+            .collect(Collectors.toList());
+
+        return new GlobalResponse<>(1, dtos);
+    }
+
+    // select Order 시 LAZY 로딩 상태의 Entity 를 모두 Fetch Join 으로 끌고와서 한방에 처리하도록 성능 개선
+    // 총 Order 는 2개, 각 Order 의 List<OrderItem> 은 2개 -> 즉 이 상태로 Fetch Join 시 Order 가 2개씩 조회되는 데이터 뻥튀기 현상 발생
+    // (RDB 는 따로 List(다) 를 처리할 수 없으니까 List(다) 만큼 Row 가 증가되서 나오는 것)
+    @GetMapping("/api/v3/orders")
+    public GlobalResponse<List<OrderDto>> orderV3() {
+        List<OrderDto> dtos = orderRepository.findAllWithItem().stream()
+            .map(order -> order.toOrderDto(order))
+            .collect(Collectors.toList());
+
+        return new GlobalResponse<>(1, dtos);
+    }
+
+    // 중복 Order 를 제거하기 위해 distinct 키워드 사용
+    // DB 의 distinct 와 다른 점으로 한 줄이 전부 같아야 중복제거를 해주는게 아닌, FK 값이 같다면 중복을 제거해준다.
+    // Fetch Join + distinct 의 조합으로 중복이 제거된 SQL 한번이 나가도록 성능이 개선되었지만 페이징에서의 문제점이 하나 있다.
+    // 일대다 Fetch Join 이 걸려있는 순간 데이터가 뻥튀기 되기 때문에 하이버네이트가 경고 로그를 남기면서 모든 데이터를 DB 에서 읽어오고,
+    // 애플리케이션 메모리 자체에서 페이징 처리를 해버린다. (잘못하면 메모리 초과 에러로 이어진다.)
+    // 또한 컬렉션 Fetch Join 은 하나만 사용하도록 하자. 두 개 이상 사용하면 데이터가 부정확하게 조회될 수 있다.
+    @GetMapping("/api/v4/orders")
+    public GlobalResponse<List<OrderDto>> orderV4() {
+        List<OrderDto> dtos = orderRepository.findAllWithItemV4().stream()
             .map(order -> order.toOrderDto(order))
             .collect(Collectors.toList());
 
--- src/main/java/com/tany/jpamaster/repository/OrderRepository.java
+++ src/main/java/com/tany/jpamaster/repository/OrderRepository.java
@@ -10,4 +10,18 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("select o from Order o join fetch o.member m join fetch o.delivery d")
     List<Order> findAllWithMemberDelivery();
+
+    @Query("select o from Order o " +
+        "join fetch o.member m " +
+        "join fetch o.delivery d " +
+        "join fetch o.orderItems oi " +
+        "join fetch oi.item i")
+    List<Order> findAllWithItem();
+
+    @Query("select distinct o from Order o " +
+        "join fetch o.member m " +
+        "join fetch o.delivery d " +
+        "join fetch o.orderItems oi " +
+        "join fetch oi.item i")
+    List<Order> findAllWithItemV4();
 }
