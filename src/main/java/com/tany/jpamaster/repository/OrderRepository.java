ca1231dbdff4cc3f6268d0492e05c396f61973bd
ByeoungJoon Jeon <juni8453@naver.com>
2022-10-16 19:58:55 +0900

    :bulb: OrderRepository findAllWithMemberDelivery() 메서드 주석 추가

--- src/main/java/com/tany/jpamaster/repository/OrderRepository.java
+++ src/main/java/com/tany/jpamaster/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.tany.jpamaster.repository;
 
 import com.tany.jpamaster.domain.Order;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    // Order 에 LAZY 로딩으로 걸려있는 Entity 인 Member, Delivery 를 한 번에 가져오게 해서 N + 1 예방
     @Query("select o from Order o join fetch o.member m join fetch o.delivery d")
     List<Order> findAllWithMemberDelivery();
 
